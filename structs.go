7e8b38b2015fe3ef775086d82c26d359b319c6ef
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-08-24 15:11:43 +0700

    update input value

--- structs.go
+++ structs.go
@@ -1,3 +1,19 @@
 package main
 
-func main() {}
+import "fmt"
+
+func main() {
+	firstName := getUserData("please enter your first name: ")
+	lastName := getUserData("please enter your last name: ")
+	birthDate := getUserData("please enter your birth date (MM/DD/YYYY): ")
+
+	fmt.Println(firstName, lastName, birthDate)
+}
+
+func getUserData(promptText string) string {
+	fmt.Println(promptText)
+
+	var value string
+	fmt.Scan(&value)
+	return value
+}
