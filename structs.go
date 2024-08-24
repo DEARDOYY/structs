9ad1607cfdca15b6f93c6d92101b3d78c83f4888
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-08-24 16:00:36 +0700

    created type struct

--- structs.go
+++ structs.go
@@ -1,12 +1,28 @@
 package main
 
-import "fmt"
+import (
+	"fmt"
+	"time"
+)
+
+// note : first character uppercase is can Outside package
+// struct type
+type user struct {
+	firstName string
+	lastName  string
+	birthDate string
+	createdAt time.Time
+}
 
 func main() {
 	firstName := getUserData("please enter your first name: ")
 	lastName := getUserData("please enter your last name: ")
 	birthDate := getUserData("please enter your birth date (MM/DD/YYYY): ")
 
+	outPutUserDetails(firstName, lastName, birthDate)
+}
+
+func outPutUserDetails(firstName, lastName, birthDate string) {
 	fmt.Println(firstName, lastName, birthDate)
 }
 
