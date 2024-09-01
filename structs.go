c6bf03ef572a68a2488ad8f92b1e75a07ad08c0f
Pinya Aungkanarungrut <88120284+DEARDOYY@users.noreply.github.com>
2024-09-01 22:12:42 +0700

    create user.go

--- structs.go
+++ structs.go
@@ -1,57 +1,27 @@
 package main
 
 import (
-	"errors"
 	"fmt"
-	"time"
+
+	"example.com/structs/user"
 )
-
-// note : first character uppercase is can Outside package
-// struct type
-type user struct {
-	firstName string
-	lastName  string
-	birthDate string
-	createdAt time.Time
-}
-
-func (u user) outPutUserDetails() {
-	fmt.Println(u.firstName, u.lastName, u.birthDate)
-}
-
-func (u user) clearUserName() {
-	u.firstName = ""
-	u.lastName = ""
-}
-
-func newUser(firstName, lastName, birthDate string) (*user, error) {
-	if firstName == "" || lastName == "" || birthDate == "" {
-		return nil, errors.New("first name, last name and birth date are requite")
-	}
-	return &user{
-		firstName: firstName,
-		lastName:  lastName,
-		birthDate: birthDate,
-		createdAt: time.Now(),
-	}, nil
-}
 
 func main() {
 	userFirstName := getUserData("please enter your first name: ")
 	userLastName := getUserData("please enter your last name: ")
 	userBirthDate := getUserData("please enter your birth date (MM/DD/YYYY): ")
 
-	var appUser *user
+	var appUser *user.User
 
-	appUser, err := newUser(userFirstName, userLastName, userBirthDate)
+	appUser, err := user.New(userFirstName, userLastName, userBirthDate)
 
 	if err != nil {
 		fmt.Println(err)
 		return
 	}
 
-	appUser.outPutUserDetails()
-	appUser.clearUserName()
+	appUser.OutPutUserDetails()
+	appUser.ClearUserName()
 }
 
 func getUserData(promptText string) string {
